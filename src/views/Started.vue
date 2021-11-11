ea975d75cd308b3a90cb50eed73e65627778d0e5
Daniel_Stoxreiter <daniel.stoxreiter@oeaw.ac.at>
2021-11-11 16:41:25 +0100

    repo restructuring

---
+++ src/views/Started.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="gotStarted">
+    <GotStarted></GotStarted>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GotStarted from "@/components/GotStarted.vue";
+
+export default {
+  name: "Started",
+  components: {
+    GotStarted,
+  },
+};
+</script>
